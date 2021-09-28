952c58a19b2bcd3de0f83dd02f4a57139365362a
pouya <pouyarezaei810@gmail.com>
2021-09-28 09:31:05 +0330

    add DetailsViewModel

---
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/ui/detail/DetailsViewModel.kt
@@ -0,0 +1,11 @@
+package com.github.pouyarezaei.basalam_t1.ui.detail
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.github.pouyarezaei.basalam_t1.util.TAG
+
+class DetailsViewModel : ViewModel() {
+    init {
+        Log.d(TAG, "DetailsViewModel: init")
+    }
+}
