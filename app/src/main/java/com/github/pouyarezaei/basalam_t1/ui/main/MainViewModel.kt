d94204d64fe0521920c2482624e149396148b0cf
pouya <pouyarezaei810@gmail.com>
2021-09-28 09:24:34 +0330

    add MainViewModel

---
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/ui/main/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.github.pouyarezaei.basalam_t1.ui.main
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.github.pouyarezaei.basalam_t1.util.TAG
+
+class MainViewModel : ViewModel() {
+    init {
+        Log.d(TAG, "MainViewModel: init")
+    }
+}
