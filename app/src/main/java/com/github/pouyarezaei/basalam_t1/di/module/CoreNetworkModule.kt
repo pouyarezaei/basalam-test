d31a2965d5c6eaedba8028f85cdd0be507896dfe
pouya <pouyarezaei810@gmail.com>
2021-09-28 09:33:17 +0330

    add dagger CoreNetworkModule

---
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/di/module/CoreNetworkModule.kt
@@ -0,0 +1,54 @@
+package com.github.pouyarezaei.basalam_t1.di.module
+
+import com.github.pouyarezaei.basalam_t1.util.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class CoreNetworkModule {
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().also {
+            it.level = HttpLoggingInterceptor.Level.BASIC
+        }
+    }
+
+    @Provides
+    fun provideGson(): Gson {
+        return GsonBuilder().create()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(gson: Gson): GsonConverterFactory {
+        return GsonConverterFactory.create(gson)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .baseUrl(BASE_URL)
+            .build()
+    }
+}
