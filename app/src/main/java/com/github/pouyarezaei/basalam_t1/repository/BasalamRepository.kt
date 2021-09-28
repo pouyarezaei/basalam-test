09c9e5feeceaa17e462afa26fa4baa25ec104ab6
pouya <pouyarezaei810@gmail.com>
2021-09-28 09:31:45 +0330

    add BasalamRepository

---
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/repository/BasalamRepository.kt
@@ -0,0 +1,24 @@
+package com.github.pouyarezaei.basalam_t1.repository
+
+import com.github.pouyarezaei.basalam_t1.data.domain.InformationDomainModel
+import com.github.pouyarezaei.basalam_t1.data.dto.InformationDataDto
+import com.github.pouyarezaei.basalam_t1.data.remote.BasalamNetworkService
+import com.github.pouyarezaei.basalam_t1.data.util.DomainMapper
+
+class BasalamRepository constructor(
+    private val basalamNetworkService: BasalamNetworkService,
+    private val domainMapper: DomainMapper<InformationDataDto, InformationDomainModel>
+) : Repository {
+    override suspend fun getAnimalPairFlower() {
+        val animals = basalamNetworkService.getInformation("animal")
+        val flowers = basalamNetworkService.getInformation("flower")
+        val animalsDomain = domainMapper.mapToDomainModel(animals.data)
+        val flowersDomain = domainMapper.mapToDomainModel(flowers.data)
+        val mergeDomain = mutableListOf<InformationDomainModel>()
+        mergeDomain.addAll(animalsDomain)
+        mergeDomain.addAll(flowersDomain)
+        val entries = mergeDomain.groupBy { it.id }.values
+        println(entries)
+    }
+
+}
