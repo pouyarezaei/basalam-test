114e9c0785cea2a2e01435368c4d11dd9a4b2761
pouya <pouyarezaei810@gmail.com>
2021-10-01 19:15:30 +0330

    add bind views logic

--- app/src/main/java/com/github/pouyarezaei/basalam_t1/MainActivity.kt
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/MainActivity.kt
@@ -1,32 +1,42 @@
 package com.github.pouyarezaei.basalam_t1
 
-import DaggerNetworkComponent
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
-import com.github.pouyarezaei.basalam_t1.data.domain.InformationMapper
-import com.github.pouyarezaei.basalam_t1.data.remote.BasalamNetworkService
-import com.github.pouyarezaei.basalam_t1.repository.BasalamRepository
-import com.github.pouyarezaei.basalam_t1.util.TAG
-import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var basalamNetworkService: BasalamNetworkService
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var toolbar: Toolbar
+    lateinit var toolbarText: TextView
+    lateinit var toolbarBackButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DaggerNetworkComponent.factory().create().inject(this)
         setContentView(R.layout.main_activity)
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
-        Log.d(TAG, "onCreate: $basalamNetworkService")
-        val repo = BasalamRepository(basalamNetworkService, InformationMapper())
-        runBlocking {
-            repo.getAnimalPairFlower()
+        toolbar = findViewById(R.id.toolbar)
+        toolbarBackButton = findViewById(R.id.toolbal_back_button)
+        toolbarText = findViewById(R.id.toolbar_title)
+        findViewById<ImageButton>(R.id.toolbar_search_button).setOnClickListener(this)
+        findViewById<ImageButton>(R.id.toolbar_option_button).setOnClickListener(this)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.toolbar_search_button -> {
+                    Toast.makeText(this, "toolbar_search_button", Toast.LENGTH_LONG).show()
+                }
+                R.id.toolbar_option_button -> {
+                    Toast.makeText(this, "toolbar_option_button", Toast.LENGTH_LONG).show()
+                    throw RuntimeException("Test Crash") // Force a crash
+                }
+            }
         }
     }
 }
