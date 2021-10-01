397e33b7d704a5674754236d24ae546606541e01
pouya <pouyarezaei810@gmail.com>
2021-10-01 19:20:56 +0330

    change return type of getAnimalPairFlower

--- app/src/main/java/com/github/pouyarezaei/basalam_t1/repository/BasalamRepository.kt
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/repository/BasalamRepository.kt
@@ -4,12 +4,13 @@ import com.github.pouyarezaei.basalam_t1.data.domain.InformationDomainModel
 import com.github.pouyarezaei.basalam_t1.data.dto.InformationDataDto
 import com.github.pouyarezaei.basalam_t1.data.remote.BasalamNetworkService
 import com.github.pouyarezaei.basalam_t1.data.util.DomainMapper
+import javax.inject.Inject
 
-class BasalamRepository constructor(
+class BasalamRepository @Inject constructor(
     private val basalamNetworkService: BasalamNetworkService,
     private val domainMapper: DomainMapper<InformationDataDto, InformationDomainModel>
 ) : Repository {
-    override suspend fun getAnimalPairFlower() {
+    override suspend fun getAnimalPairFlower(): MutableList<List<InformationDomainModel>> {
         val animals = basalamNetworkService.getInformation("animal")
         val flowers = basalamNetworkService.getInformation("flower")
         val animalsDomain = domainMapper.mapToDomainModel(animals.data)
@@ -18,7 +19,7 @@ class BasalamRepository constructor(
         mergeDomain.addAll(animalsDomain)
         mergeDomain.addAll(flowersDomain)
         val entries = mergeDomain.groupBy { it.id }.values
-        println(entries)
+        return entries.toMutableList()
     }
 
 }
