8fec1e7150946c1082a4560845fda4fe1b87db46
pouya <pouyarezaei810@gmail.com>
2021-10-01 19:09:49 +0330

    create dagger SubcomponentsModule

---
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/di/module/SubcomponentsModule.kt
@@ -0,0 +1,8 @@
+package com.github.pouyarezaei.basalam_t1.di.module
+
+import com.github.pouyarezaei.basalam_t1.di.subcomponent.MainFragmentComponent
+import dagger.Module
+
+@Module(subcomponents = [MainFragmentComponent::class])
+class SubcomponentsModule {
+}
