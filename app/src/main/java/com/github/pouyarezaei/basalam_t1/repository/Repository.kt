4af1330c939a23b8c4c3768b4adf4cc274eb7f70
pouya <pouyarezaei810@gmail.com>
2021-10-01 19:10:22 +0330

    change return type of getAnimalPairFlower method

--- app/src/main/java/com/github/pouyarezaei/basalam_t1/repository/Repository.kt
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/repository/Repository.kt
@@ -1,5 +1,7 @@
 package com.github.pouyarezaei.basalam_t1.repository
 
+import com.github.pouyarezaei.basalam_t1.data.domain.InformationDomainModel
+
 interface Repository {
-    suspend fun getAnimalPairFlower()
+    suspend fun getAnimalPairFlower(): MutableList<List<InformationDomainModel>>
 }
