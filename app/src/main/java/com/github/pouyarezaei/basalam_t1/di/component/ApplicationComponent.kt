c5de4a466b31dede017347190f2eb0c30408a9a9
pouya <pouyarezaei810@gmail.com>
2021-10-01 19:22:32 +0330

    add mainFragmentComponent factory

--- app/src/main/java/com/github/pouyarezaei/basalam_t1/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/di/component/ApplicationComponent.kt
@@ -1,16 +1,32 @@
 package com.github.pouyarezaei.basalam_t1.di.component
 
 import com.github.pouyarezaei.basalam_t1.BaseApplication
-import com.github.pouyarezaei.basalam_t1.MainActivity
+import com.github.pouyarezaei.basalam_t1.di.module.*
+import com.github.pouyarezaei.basalam_t1.di.subcomponent.MainFragmentComponent
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
-@Component
+@Singleton
+@Component(
+    modules = [
+        ViewModelModule::class,
+        SubcomponentsModule::class,
+        CoreNetworkModule::class,
+        NetworkServiceModule::class,
+        MapperModule::class,
+        Binder::class
+    ],
+
+
+    )
 interface ApplicationComponent {
     @Component.Factory
     interface Factory {
         fun create(@BindsInstance application: BaseApplication): ApplicationComponent
     }
+
+    fun mainFragmentComponent(): MainFragmentComponent.Factory
 
     fun inject(baseApplication: BaseApplication)
 }
