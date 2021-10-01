5f5de422ab2c4fc463ea79492e0d62b3545f6f3b
pouya <pouyarezaei810@gmail.com>
2021-10-01 19:11:20 +0330

    remove singleton annotation from provideBasalamNetworkService

--- app/src/main/java/com/github/pouyarezaei/basalam_t1/di/module/NetworkServiceModule.kt
+++ app/src/main/java/com/github/pouyarezaei/basalam_t1/di/module/NetworkServiceModule.kt
@@ -1,11 +1,16 @@
+package com.github.pouyarezaei.basalam_t1.di.module
+
 import com.github.pouyarezaei.basalam_t1.data.remote.BasalamNetworkService
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 
 @Module
 class NetworkServiceModule {
+
+    @Singleton
     @Provides
     fun provideBasalamNetworkService(retrofit: Retrofit): BasalamNetworkService {
         return retrofit.create(BasalamNetworkService::class.java)
